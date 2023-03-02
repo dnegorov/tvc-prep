fb3ea2cd5fd9f1e30e1e1ecd1adefae89962fad2
Denis Egorov <dnegorov@gmail.com>
2023-03-02 15:20:32 +0300

    fix

--- create-iso.sh
+++ create-iso.sh
@@ -16,8 +16,8 @@ ISO_LABEL="$HOST_NAME"
 ISO_FILE_NAME="$ISO_OUTPUT_DIR""/""$ISO_LABEL"".iso"
 
 mkdir -p "$ISO_OUTPUT_DIR"
-rm -rf "$ISO_OUTPUT_DIR""/*"
 
 echo "Create ISO for: ""$HOST_NAME"
 echo "Output file:    ""$ISO_FILE_NAME"
+echo
 mkisofs -joliet -rock -volid "$ISO_LABEL" -output "$ISO_FILE_NAME" ./*.*
